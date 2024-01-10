07b5d810d2398b8ffda716efc3796bdceefef966
sufiAzarquiel <alumno.745709@ies-azarquiel.es>
2024-01-10 13:29:24 +0100

    empezar ejercicio 2

---
+++ 02-manipula_arrays.js
@@ -0,0 +1,11 @@
+const filtrarNumerosPares = (arr) => arr.filter((el) => el%2 == 0)
+/*let a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+a = filtrarNumerosPares(a)
+console.log(a)*/
+
+const duplicarElementos = (arr) => arr.map((el) => el*2)
+/*let a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+a = duplicarElementos(a)
+console.log(a)*/
+
+const convertir
